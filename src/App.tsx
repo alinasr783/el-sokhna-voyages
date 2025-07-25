e7f73d46dca1f1652c36bc01105778b46ad941af
alinasr783 <alinasreldin783@gmail.com>
2025-07-25 05:41:56 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { LanguageProvider } from "@/contexts/LanguageContext";
 import { AuthProvider } from "@/contexts/AuthContext";
+
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+
 import { Home } from "./pages/Home";
 import { YachtDetail } from "./pages/YachtDetail";
 import { YachtsList } from "./pages/YachtsList";
@@ -15,48 +18,40 @@ import { LocationDetail } from "./pages/LocationDetail";
 import { AdminLogin } from "./pages/AdminLogin";
 import { AdminPanel } from "./pages/AdminPanel";
 import NotFound from "./pages/NotFound";
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
-const App = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [location.pathname]);
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <LanguageProvider>
-          <AuthProvider>
-            <Toaster />
-            <Sonner />
-            <BrowserRouter>
-              <div className="min-h-screen flex flex-col">
-                <Header />
-                <main className="flex-1">
-                  <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/yachts" element={<YachtsList />} />
-                    <Route path="/yacht/:id" element={<YachtDetail />} />
-                    <Route path="/locations" element={<LocationsList />} />
-                    <Route path="/location/:id" element={<LocationDetail />} />
-                    <Route path="/admin-login" element={<AdminLogin />} />
-                    <Route path="/admin" element={<AdminPanel />} />
-                    <Route path="*" element={<NotFound />} />
-                  </Routes>
-                </main>
-                <Footer />
-              </div>
-            </BrowserRouter>
-          </AuthProvider>
-        </LanguageProvider>
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
-};
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <LanguageProvider>
+        <AuthProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <div className="min-h-screen flex flex-col">
+              <Header />
+              <main className="flex-1">
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/yachts" element={<YachtsList />} />
+                  <Route path="/yacht/:id" element={<YachtDetail />} />
+                  <Route path="/locations" element={<LocationsList />} />
+                  <Route path="/location/:id" element={<LocationDetail />} />
+                  <Route path="/admin-login" element={<AdminLogin />} />
+                  <Route path="/admin" element={<AdminPanel />} />
+                  
+                  {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </main>
+              <Footer />
+            </div>
+          </BrowserRouter>
+        </AuthProvider>
+      </LanguageProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
 
 export default App;
